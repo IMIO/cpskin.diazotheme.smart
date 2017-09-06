4bb85dda44bf05fa94f3d0f48cd7def72eed1314
Christophe Boulanger <christophe.boulanger@imio.be>
2017-09-06 15:39:59 +0200

    Back to development: 0.5.16

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.smart package."""
 
-version = '0.5.15'
+version = '0.5.16.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
