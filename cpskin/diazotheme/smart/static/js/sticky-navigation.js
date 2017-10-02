ae72a94719fd1bc483aea755a913068046e6a627
Olivier Snickers <olivier.snickers@imio.be>
2017-10-02 15:43:07 +0200

    change css

--- cpskin/diazotheme/smart/static/js/sticky-navigation.js
+++ cpskin/diazotheme/smart/static/js/sticky-navigation.js
@@ -1,80 +1,26 @@
-$(document).ready(function(){ 
-(function(w,d,undefined){
-	var el_html = d.documentElement,
-		el_body = d.getElementsByTagName('body')[0],
-		header = d.getElementById('top-navigation-inner'),
-		header_miniSite = d.getElementById('minisite-navigation-inner'),
-		lastScroll = w.pageYOffset || el_body.scrollTop;
-		MinilastScroll = w.pageYOffset || el_body.scrollTop;
-	
-		menuIsStuck = function(triggerElement, wScrollTop, lastScroll) {
-			var regexp		= /(nav\-is\-stuck)/i,
-				classFound	= el_html.className.match( regexp ),
-				navHeight	= header.offsetHeight,
-				bodyRect	= el_body.getBoundingClientRect(),
-				scrollValue	= triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight  : 100,
-				scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
-			if (window.matchMedia("(min-width: 1024px)").matches){
-				
-				if (wScrollTop > scrollValFix && !classFound && wScrollTop > lastScroll ) {
-					el_html.className = el_html.className + ' nav-is-stuck';
-				}
-				
-				// if nav-is-stuck class exists
-				if ( (classFound &&  wScrollTop < lastScroll) || w.pageYOffset == 0) {
-					el_html.className = el_html.className.replace( regexp, '' );
-				}				
-			}	
-		};
-		
-		if ( header_miniSite !== null ) {
-				miniSiteMenuIsStuck = function(triggerElement, wScrollTop, MinilastScroll) {
-	
-				var regexp		= /(nav\-is\-Substuck)/i,
-					classFound	= el_html.className.match( regexp ),
-					navHeight	= header_miniSite.offsetHeight,
-					bodyRect	= el_body.getBoundingClientRect(),
-					scrollValue	= triggerElement ? triggerElement.getBoundingClientRect().top - bodyRect.top - navHeight  : 100,
-					scrollValFix = classFound ? scrollValue : scrollValue + navHeight;
-					if (window.matchMedia("(min-width: 1024px)").matches) {
-                        if ( d.getElementsByClassName('in-minisite-in-portal') || d.getElementsByClassName('in-minisite-out-portal') ) {
-					
-							if (wScrollTop > scrollValFix && !classFound && wScrollTop > MinilastScroll) {
-								el_html.className = el_html.className + ' nav-is-Substuck';
-							}
-			
-							// if nav-is-Substuck class exists
-							if ( (classFound && wScrollTop < MinilastScroll) || w.pageYOffset == 0) {
-								el_html.className = el_html.className.replace( regexp, '' );
-							}
-						}
-          }	
-			}
-    };
-	
-    
-		onScrolling = function() {
-      //this function fires menuIsStuck()…
-		var wScrollTop = w.pageYOffset || el_body.scrollTop;
-		
-			if (header != null) {			
-        menuIsStuck( d.getElementById('header'), wScrollTop, lastScroll );
-				lastScroll = wScrollTop;
-			}
-			
-			if (header_miniSite !== null) {
-          miniSiteMenuIsStuck( d.getElementById('header_miniSite'), wScrollTop, MinilastScroll );
-          MinilastScroll = wScrollTop;
-      }
-			
-		};
-		
-	// when you scroll, fire onScrolling() function
-	w.addEventListener('scroll', function(){
-		w.requestAnimationFrame( onScrolling );
-	});
-
-
-}(window, document));
-
-});
+jQuery(document).ready(function($) {
+ $(window).scroll(function(e) {
+   if ($("#portal-column-content").length == 0) return;
+   if ($("#mobnav-btn").is(":visible")) return;
+   var scroller_anchor = $("#portal-column-content").offset().top;
+   if ($(this).scrollTop() >= scroller_anchor && !$('html').hasClass('nav-is-stuck'))
+   {   // Fix panel at the top of the screen when users scrolls below anchor.
+     $('html').addClass('nav-is-stuck');
+     $('html').addClass('nav-is-Substuck');
+     top_position = $('#top-navigation').height();
+     if ($('body').hasClass('in-minisite-out-portal'))
+     top_position = 0;
+     $('.container-minisite-globalnav-logo').css({
+       'position': 'fixed',
+       'top': top_position + 'px'          });      }
+     else if ($(this).scrollTop() < scroller_anchor && $('html').hasClass('nav-is-stuck'))
+     {   // Put it back to its original position when users scrolls back
+       $('html').removeClass('nav-is-stuck');
+       $('html').removeClass('nav-is-Substuck');
+       $('.container-minisite-globalnav-logo').css({
+         'position': 'relative',
+         'top': '0px'
+         });
+       }
+       });
+ });
