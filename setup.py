6fe9c74cafabbc1f20b35b8ddb7bb5b49d980b0b
maud gennart <maud.gennart@imio.be>
2017-11-28 17:48:48 +0100

    Preparing release 0.6.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.smart package."""
 
-version = '0.6.8.dev0'
+version = '0.6.8'
 
 from setuptools import find_packages
 from setuptools import setup
