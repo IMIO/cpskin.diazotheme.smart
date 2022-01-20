169525b43b0a32b9420a7870812aa6df37b3bbf8
Laurent Lasudry <lasudry@gmail.com>
2022-01-20 11:25:01 +0100

    Preparing release 0.6.28

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.smart package."""
 
-version = '0.6.28.dev0'
+version = '0.6.28'
 
 from setuptools import find_packages
 from setuptools import setup
